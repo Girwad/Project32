2cde8cbb13ea025b90d4d88d5987d734e15b23d8
Girwad <72423701+Girwad@users.noreply.github.com>
2020-12-20 20:24:24 -0600

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,42 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.Visibility = 255;
+      }
+
+      score(){
+        if (this.Visibility<0 && this.Visibility>-105){
+           score++;
+        }
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        console.log(this.body.speed);
+        if (this.body.speed < 3){
+          push();
+          translate(pos.x, pos.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(0,0,this.width, this.height);
+          pop();
+        }
+        else{
+          push();
+          World.remove(world, this.body);
+          this.Visibility = this.Visibility - 5;
+          tint(255, this.Visibility);
+          pop();
+        }
+      }
+
+     }
